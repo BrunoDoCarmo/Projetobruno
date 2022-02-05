ac8eed8e022f973a0306595d5344f23130d455f8
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2022-02-05 07:54:51 -0300

    vue

---
+++ projeto/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    <AppTopNavigator />
+  </div>
+</template>
+
+<script>
+import AppTopNavigator from "../components/AppTopNavigation.vue";
+// import '../App.css';
+export default {
+  name: 'Home',
+  components: {
+    AppTopNavigator,
+  }
+};
+</script>
