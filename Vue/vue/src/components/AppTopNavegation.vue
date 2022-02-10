5b37e894695109f34edfa21b81102e158be4e72b
Bruno Do Carmo <bruno_stcarmo3@outlook.com>
2022-02-10 17:55:26 -0300

    xx

---
+++ Vue/vue/src/components/AppTopNavegation.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <h1>Ola</h1>
+  </nav>
+</template>
+<script>
+export default {
+  name: "AppTopNavegation",
+  components: {
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
---
+++ Vue/vue/src/views/AppHome.vue
@@ -0,0 +1,12 @@
+<template>
+  <AppTopNavigation/>
+</template>
+<script>
+import AppTopNavegation from '../components/HelloWorld.vue'
+export default {
+  name: "AppHome",
+  components: {
+    AppTopNavegation
+  }
+}
+</script>
